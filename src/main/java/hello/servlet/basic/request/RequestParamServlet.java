06a250fe89d4487b515d5c65d4d264de23439af8
HANJIYEONN <onedelay1@naver.com>
2021-04-18 20:32:18 +0900

    스프링 MVC 1편 - 백엔드 웹 개발 핵심 기술 / HTTP 요청 데이터 - API 메시지 바디 - JSON

--- src/main/java/hello/servlet/basic/request/RequestParamServlet.java
+++ src/main/java/hello/servlet/basic/request/RequestParamServlet.java
@@ -28,6 +28,20 @@ public class RequestParamServlet  extends HelloServlet {
         System.out.println("[전체 파라미터 조회] - end");
         System.out.println();
         System.out.println("[단일 파라미터 조회]");
+        String username = request.getParameter("username");
+        String age = request.getParameter("age");
 
+        System.out.println("username = " + username);
+        System.out.println("age = " + age);
+        System.out.println();
+
+        System.out.println("[이름이 같은 복수 파라미터 조회]");
+        String[] usernames = request.getParameterValues("username");
+        for (String name : usernames) {
+            System.out.println("username = " + name);
+
+        }
+
+        response.getWriter().write("ok");
     }
 }
