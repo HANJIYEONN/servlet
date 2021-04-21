15c672231b8e0acaa451b7b061a236316c3f242d
hanjiyeon <onedelay1@naver.com>
2021-04-21 12:57:06 +0900

    서블릿, JSP, MVC 패턴 / 회원 관리 앱 애플리케이션 요구사항 1

---
+++ src/test/java/hello/servlet/domian/Member/MemberRepositoryTest.java
@@ -0,0 +1,29 @@
+package hello.servlet.domian.Member;
+
+import hello.servlet.domain.member.Member;
+import hello.servlet.domain.member.MemberRepository;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+
+class MemberRepositoryTest {
+
+    MemberRepository memberRepository = MemberRepository.getInstance();
+
+    @AfterEach
+    void afterEach(){
+        memberRepository.clearStore();
+    }
+
+    @Test
+    void save(){
+        //given
+        Member member = new Member("hello", 20);
+        //when
+        Member savedMember = memberRepository.save(member);
+        //then
+        Member findMember = memberRepository.findById(savedMember.getId());
+        Assertions.assertThat(findMember).isEqualTo(savedMember);
+    }
+
+}
